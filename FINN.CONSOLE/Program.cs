f5b06b77931399ae8107006823060562a1662eaf
snailya2005 <19658624+Snailya@users.noreply.github.com>
2022-07-14 17:41:20 +0800

    init: smaple code for cad drawing

---
+++ FINN.CONSOLE/Program.cs
@@ -0,0 +1,17 @@
+﻿using FINN.CAD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINN.CONSOLE
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			CADHelper.Place();
+		}
+	}
+}
