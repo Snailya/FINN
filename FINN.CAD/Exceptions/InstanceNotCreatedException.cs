aebc2674ae5cd95aef0f2320d639adaf085d55a0
snailya2005 <19658624+Snailya@users.noreply.github.com>
2022-07-15 16:38:46 +0800

    refactor: move model into FINN.COMMON, prepare environemnt for AutoCAD in and out of process assembly

---
+++ FINN.CAD/Exceptions/InstanceNotCreatedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FINN.CAD
+{
+    [Serializable]
+    internal class InstanceNotCreatedException : System.Exception
+    {
+        public InstanceNotCreatedException()
+        {
+        }
+
+        public InstanceNotCreatedException(string message) : base(message)
+        {
+        }
+
+        public InstanceNotCreatedException(string message, System.Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InstanceNotCreatedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
