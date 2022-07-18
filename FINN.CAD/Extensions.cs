5a3fcfd26abbfddc399a14f9f38f41321131967d
snailya2005 <19658624+Snailya@users.noreply.github.com>
2022-07-18 17:35:25 +0800

    feat: create single node from ProcessNode object - user input prompt when Data is null

---
+++ FINN.CAD/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINN.CAD
+{
+	internal static class Extensions
+	{
+		public static Models.ProcessNode ToProcessNode(this FINN.ProcessNode node, double x, double y)
+		{
+			return new Models.ProcessNode()
+			{
+				Title = node.Name,
+				Body = string.Join("\n", node.OpertaionMeta),
+
+				XLength = node.ShapeMeta.Length,
+				YLength = node.ShapeMeta.Width,
+
+				Position = new Autodesk.AutoCAD.Geometry.Point2d(x, y)
+			};
+		}
+	}
+}
+
